0927e8885afaa5478cfe2d46f480e46bc1135807
laurynaslopata <laurynas.lopata@epfl.ch>
2024-03-24 14:05:20 +0100

    Started frontend cleanup

--- frontend/src/sections/TokenViewer.tsx
+++ frontend/src/sections/TokenViewer.tsx
@@ -26,14 +26,6 @@ const COLORS = [
   "bg-teal-200",
 ];
 
-const PRICING: Record<string, BN> = {
-  "gpt-4": BN("0.03").div(1000),
-  "gpt-4-1106-preview": BN("0.01").div(1000),
-  "gpt-4-32k": BN("0.03").div(1000),
-  "gpt-3.5-turbo": BN("0.0010").div(1000),
-  "gpt-3.5-instruct": BN("0.0015").div(1000),
-};
-
 function encodeWhitespace(str: string) {
   let result = str;
 
